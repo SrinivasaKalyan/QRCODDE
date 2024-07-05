9dc6d72dcced50dbd3276bbb9ce595d3c54016b9
Kalyan Kanchumarthi <109733865+SrinivasaKalyan@users.noreply.github.com>
2024-07-05 14:58:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PIL import Image
 from streamlit_lottie import st_lottie
 import json
 
-if st.button("ℹ️", key="info_button"):
+with st.expander("Kalyan Kanchumarthi"):
         st.write("""
 Create a QR CODE of your own...!!
 - Enter the required text or link and
